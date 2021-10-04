03139dcaae8b1953609aef2eaacd8ec1c69cdedb
Aleksei Dmitriev <alexey@ioncloud.ru>
2021-10-04 13:09:55 +0200

    fix: client

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -305,7 +305,7 @@ const Header = ({ border }) => {
         FTM / WFTM Station
       </div>
       <div className={styles.menuSeparator} />
-      {account?.toLowerCase() === ADMIN_ADDRESS[chainId].toLowerCase()
+      {account?.toLowerCase() === ADMIN_ADDRESS[chainId]?.toLowerCase()
         ? [
             <div key={0} className={styles.menuItem} onClick={addMod}>
               Add Mod
