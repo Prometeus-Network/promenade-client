5926b8ea2134f888466879f5be2c4a7651b89bcc
Gosha Skryuchenkov <gosha.skryuchenkov@gmail.com>
2021-10-04 13:50:26 +0300

    feat: mainnet addresses

--- src/components/app.js
+++ src/components/app.js
@@ -43,7 +43,7 @@ const App = () => {
       } else if (chainId === ChainId.FANTOM_TESTNET) {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const oracle = new ethers.Contract(
-          '0xe04676B9A9A2973BCb0D1478b5E1E9098BBB7f3D',
+          '0xf4766552D15AE4d256Ad41B6cf2933482B0680dc',
           [
             {
               inputs: [],
